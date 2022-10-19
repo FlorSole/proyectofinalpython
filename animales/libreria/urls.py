c58f0f6ac0f18a0fb5054b29bf43f53539d23e33
Matias Rodriguez <ariirod92@gmail.com>
2022-10-19 17:36:09 -0300

    AHHH

--- animales/libreria/urls.py
+++ animales/libreria/urls.py
@@ -3,8 +3,9 @@ from . import views
 
 urlpatterns =   [
 
-    path('nosotros', views.nosotros, name='nosotros'),
     path('', views.index, name='index'),
+    path('nosotros', views.nosotros, name='nosotros'),
+    path('index', views.index, name='index'),
     path('peluditos', views.peluditos, name='peluditos'),
 
 ]
--- animales/libreria/views.py
+++ animales/libreria/views.py
@@ -3,11 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+def inicio(request):
+    return HttpResponse("<h1> Bienvenido </h1>")
+
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
 
 def index(request):
-    return render(request, 'paginas/index.html')
+    return render(request, 'index.html')
 
 def peluditos(request):
     return render(request, 'peluditos.html')
